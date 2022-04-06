ee0ad3c4ebed7be2a71d5318992a45319a4511e8
kookie-dev <cjk921211@gmail.com>
2022-04-06 15:06:58 +0900

    Initial Commit

---
+++ KMOOC/KMOOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMOOCApp.swift
+//  KMOOC
+//
+//  Created by MacBook on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct KMOOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
